6831e0500e91259a49dc5e958d39d720bae65218
raywiis <rejus.paukst@gmail.com>
2023-01-20 00:38:55 +0200

    Explore scaling

--- index.ts
+++ index.ts
@@ -10,8 +10,6 @@ const ctx = canvas.getContext('2d');
 canvas.width = appDiv.offsetWidth;
 canvas.height = appDiv.offsetHeight;
 
-ctx.strokeRect(0, 0, 100, 100);
-
 let pan = false;
 let startX, startY;
 let setX = 0,
@@ -22,6 +20,18 @@ canvas.addEventListener('mousedown', (event) => {
   startX = event.offsetX;
   startY = event.offsetY;
 });
+
+const draw = () => {
+  ctx.resetTransform();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.scale(scale, scale);
+  ctx.translate(setX, setY);
+  ctx.strokeRect(100, 100, 200, 200);
+  ctx.strokeRect(0, 0, 400, 400);
+};
+
+draw();
+
 canvas.addEventListener('mouseup', (move) => {
   if (!pan) {
     return;
@@ -43,24 +53,35 @@ canvas.addEventListener('mousemove', (move) => {
   if (!pan) {
     return;
   }
-  ctx.resetTransform();
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.scale(scale, scale);
 
-  ctx.translate(
-    setX + (move.offsetX - startX) * (1 / scale),
-    setY + (move.offsetY - startY) * (1 / scale)
-  );
-  ctx.strokeRect(0, 0, 100, 100);
+  setX += (move.offsetX - startX) * (1 / scale);
+  setY += (move.offsetY - startY) * (1 / scale);
+  startX = move.offsetX;
+  startY = move.offsetY;
+  draw();
 });
 
 canvas.addEventListener('wheel', (event) => {
-  console.log(event.deltaY);
-  scale += event.deltaY * 0.001;
+  const delta = event.deltaY * 0.001;
 
-  ctx.resetTransform();
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.scale(scale, scale);
-  ctx.translate(setX, setY);
-  ctx.strokeRect(0, 0, 100, 100);
+  const prescale = scale;
+  const prescaleX = event.offsetX;
+  const prescaleY = event.offsetY;
+
+  scale -= delta;
+
+  const scaleCoff = scale / prescale;
+  const scaleX = prescaleX * scaleCoff;
+  const scaleY = prescaleY * scaleCoff;
+  const dx = prescaleX - scaleX;
+  const dy = prescaleY - scaleY;
+
+  // setX /= scaleCoff;
+  setX += dx;
+
+  console.log({ prescaleX, prescaleY, dx });
+
+  // console.log(x);
+  // setY += dy;
+  draw();
 });
